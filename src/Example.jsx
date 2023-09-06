4ce119d8dd217cacd8bdd1306a273d01556be97c
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2023-09-06 11:28:39 +0700

    eleven

--- src/Example.jsx
+++ src/Example.jsx
@@ -3,11 +3,7 @@ import React from "react";
 const Example = () => {
     return (
         <>
-<<<<<<< HEAD
-            <div class="text-3xl font-bold underline">Hưng đẹp trai</div>
-=======
             <div class="text-3xl font-bold underline">Nhóm trưởng Hưng</div>
->>>>>>> 2a1e86b69f349c814ea7146b62431c2eb0a9348a
             <div class="text-3xl font-bold underline">Tài</div>
             <div class="text-3xl font-bold underline">Thắng</div>
         </>

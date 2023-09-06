c3322329021f8698a99e319a1a18f5b1ac1c6f74
nguyentuanhung123 <100372664+nguyentuanhung123@users.noreply.github.com>
2023-09-06 10:59:27 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+   // These paths are just examples, customize them to match your project structure
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},

b44a00205cd6838ba3c95a1bcd076a2c750c3314
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2023-09-06 11:06:09 +0700

    sua lai example

--- src/Example.jsx
+++ src/Example.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Example = () => {
     return (
-        <div class="text-3xl font-bold underline">Hung</div>
-        <div class="text-3xl font-bold underline">Tài</div>
-        <div class="text-3xl font-bold underline">Thắng</div>
+        <>
+            <div class="text-3xl font-bold underline">Hưng</div>
+            <div class="text-3xl font-bold underline">Tài</div>
+            <div class="text-3xl font-bold underline">Thắng</div>
+        </>
     )
 }
 

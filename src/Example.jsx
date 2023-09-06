2a1e86b69f349c814ea7146b62431c2eb0a9348a
nguyentuanhung123 <100372664+nguyentuanhung123@users.noreply.github.com>
2023-09-06 11:17:54 +0700

    edit example hung

--- src/Example.jsx
+++ src/Example.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Example = () => {
     return (
         <>
-            <div class="text-3xl font-bold underline">Hưng</div>
+            <div class="text-3xl font-bold underline">Nhóm trưởng Hưng</div>
             <div class="text-3xl font-bold underline">Tài</div>
             <div class="text-3xl font-bold underline">Thắng</div>
         </>

c59ac411a67fd67d8ce83e5fa01452ff53224737
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2023-09-06 11:30:46 +0700

    thirteen

--- src/Example.jsx
+++ src/Example.jsx
@@ -4,7 +4,7 @@ const Example = () => {
     return (
         <>
             <div class="text-3xl font-bold underline">Nhóm trưởng Hưng</div>
-            <div class="text-3xl font-bold underline">Tài</div>
+            <div class="text-3xl font-bold underline">Tài nhóm phó</div>
             <div class="text-3xl font-bold underline">Thắng</div>
         </>
     )

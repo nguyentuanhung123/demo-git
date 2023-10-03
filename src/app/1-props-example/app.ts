2f8fcdb54550ed9b8ef28128c4be1e7af23890bf
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2023-10-03 22:51:03 +0700

    add folder ts

---
+++ src/app/1-props-example/app.ts
@@ -0,0 +1,25 @@
+let variable = "hello";
+
+variable = "hi";
+
+// let age = 18
+
+let ageWithType: number;
+
+ageWithType = 18;
+
+let testString: string;
+
+testString = "hello";
+
+let testBoolean: boolean;
+
+testBoolean = false;
+
+let testStringOrNumber: string | number;
+
+testStringOrNumber = 10;
+testStringOrNumber = "10";
+// testStringOrNumber = []
+
+//ARRAY
